9160d56019e6296abad064e428431362e51dc500
manuhostin <hostimmanu@gmail.com>
2025-02-04 15:15:52 -0300

    manu

--- alunos/models.py
+++ alunos/models.py
@@ -6,3 +6,16 @@ class  Estado(models.Model):
 
      def __str__(self):
           return f"{self.nome} ({self.sigla})"
+
+class Cidade(models.Model): 
+    nome = models.CharField(max_length=50)
+    estado = models.ForeignKey(Estado, on_delete=models.PROTECT)
+
+    def _str_(self):
+         return f"{self.nome} ({Estado.sigla})"
+
+class Pessoa(models.Model):
+     nome = models.CharField(max_length=100)
+     telefone = models.CharField(max_length=11)
+     email = models.EmailField(max_length=50)
+     datanasc = models.DateField(max_length=6)
